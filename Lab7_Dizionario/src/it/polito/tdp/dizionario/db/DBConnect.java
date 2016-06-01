347e4e63a4fed3d931ff7fb896665bc5bdd848f0
Salvatore Caristo <s196241@studenti.polito.it>
2016-06-01 18:07:37 +0200

    Niente

--- Lab7_Dizionario/src/it/polito/tdp/dizionario/db/DBConnect.java
+++ Lab7_Dizionario/src/it/polito/tdp/dizionario/db/DBConnect.java
@@ -7,7 +7,7 @@ import java.sql.SQLException;
 
 public class DBConnect {
 
-	static private final String jdbcUrl = "jdbc:mysql://localhost/dizionario?user=root&password=root";
+	static private final String jdbcUrl = "jdbc:mysql://localhost/dizionario?user=root&password=";
 	static private DBConnect instance = null;
 
 	private DBConnect() {
